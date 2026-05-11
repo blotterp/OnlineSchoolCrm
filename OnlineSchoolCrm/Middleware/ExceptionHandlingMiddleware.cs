92b37e0dc41ca34c9684406d873328de26490001
Ekaterina Saveleva <esaveleva404@gmail.com>
2026-05-12 01:44:56 +0300

    Start exception handling middleware

---
+++ OnlineSchoolCrm/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using System.Net;
+using System.Text.Json;
+
+
+namespace OnlineSchoolCrm.Middleware;
+public sealed class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionHandlingMiddleware(RequestDelegate next)
+    {  
+        _next = next; 
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        await _next(context);
+    }
+        
+}
